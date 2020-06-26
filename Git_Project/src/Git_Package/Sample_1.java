17986755a07a3bd7f28b7d3dad1b0274bf9ec1d4
sujith <sujith@DESKTOP-GR2M6BJ>
2020-06-26 04:35:05 -0700

    C2

--- Git_Project/src/Git_Package/Sample_1.java
+++ Git_Project/src/Git_Package/Sample_1.java
@@ -3,8 +3,9 @@ package Git_Package;
 public class Sample_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("hi hello Namaste");
+		System.out.println("This is selenium git");
 
 	}
 
